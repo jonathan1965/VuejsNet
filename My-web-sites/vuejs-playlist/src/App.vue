591695d652e4b308218f532f6d89de52fc7b73e5
kalisa jonathan <jonakarisa@yahoo.fr>
2020-05-10 09:43:12 +0200

    foo

---
+++ My-web-sites/vuejs-playlist/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div >
+  <add-blog></add-blog>
+  </div>
+ 
+</template>
+<script>
+import addBlog from './components/addBlog'
+export default {
+     components:
+     {
+       'add-blog':addBlog
+     },
+  data () {
+    
+    return {
+
+    }
+
+    },
+    methods:
+    {
+
+    }
+
+}
+</script>
+<style  scoped>
+
+body{
+  margin:0;
+  font-family: 'Nunito SemiBold';
+
+}
+
+</style>
